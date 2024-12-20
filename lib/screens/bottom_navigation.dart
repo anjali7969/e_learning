87cbe6d536bed91fcb91ca5c458eb68beee6db41
anjali7969 <123344183+anjali7969@users.noreply.github.com>
2024-12-20 11:18:17 +0545

    Dashboard

---
+++ lib/screens/bottom_navigation.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class BottomNavigationView extends StatefulWidget {
+  const BottomNavigationView({super.key});
+
+  @override
+  State<BottomNavigationView> createState() => BottomNavigationViewState();
+}
+
+class BottomNavigationViewState extends State<BottomNavigationView> {
+  int selectedIndex = 0;
+
+  List<Widget> lstBottomScreen = [
+    // const HomeScreen(),
+    // const learningScreen(), //PascalCase
+    // const ProfileScreen(),
+    // const NoticeScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dashboard'),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: lstBottomScreen[selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school), // Updated to represent Learning
+            label: 'Learning',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications), // Updated to represent Notice
+            label: 'Notice',
+          ),
+        ],
+        currentIndex: selectedIndex,
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
